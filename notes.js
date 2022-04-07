b46f401a320060e237852d5bcbaef2d6059f88d8
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-04-07 10:08:25 -0500

    Re-vamped file structure and begin class creation, closing this branch and switching to class create branch

--- notes.js
+++ notes.js
@@ -48,3 +48,6 @@ const word = 'fox';
 
 console.log(`The word "${word}" ${sentence.includes(word) ? 'is' : 'is not'} in the sentence`);
 // expected output: "The word "fox" is in the sentence"
+
+// MOCK implementation tests
+// Watch videos on MOCK implementation
