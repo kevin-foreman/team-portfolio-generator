e720137f4c37ccd3eacaaaf1e498d71cdddf35a0
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-04-09 11:56:21 -0500

    Put some polish on the tests, everything looks good, moving on to fix the prompts

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,29 +1,26 @@
 // This is the parent class test
 
-jest.mock('../lib/Employee');
 
-
-const employee = require('../lib/Employee');
+const Employee = require('../lib/Employee');
 const { expect } = require('@jest/globals');
+
+// jest.mock('../lib/Employee');
 
 
 test('creaetes an employee object', () => {
-    const employee = new Employee('Class of Employee');
+    const employee = new Employee('Steve', 99448, 'mai@email.mail', 'Employee');
 
-    expect(employee.name).toBe('Class of Employee');
+    expect(employee.name).toBe('Steve');
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
-    expect(employee.role).toEqual(expect.any(String));
-    expect(employee.github).toEqual(expect.any(String));
 
-    console.log(employee.school);
-
+    console.log(employee.id);
 });
 
 
 test("getRole () returns 'employee' ", () => {
-    const employee = new Employee('Class of Employee');
+    const employee = new Employee('Employee', 99448, 'mai@email.mail', 'Employee');
     
-    expect(employee.getRole()).toEqual(expect.any(Object));
+    expect(employee.getRole()).toEqual(expect.any(String));
 
 });
--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -2,15 +2,16 @@
 // Tests to verify the right kind of input is recieved
 // Tests to verify properties are inherited from Employee class
 
-jest.mock('../lib/Employee');
-
 
 const Engineer = require('../lib/Engineer');
 const { expect } = require('@jest/globals');
 
+// jest.mock('../lib/Employee');
+
+
 
 test('creaetes an engineer object', () => {
-    const engineer = new Engineer(name = 'Steve');
+    const engineer = new Engineer('Steve', 4567, 'mail@mail.mail', 'Engineer', 'github.com');
 
     expect(engineer.name).toBe('Steve');
     expect(engineer.id).toEqual(expect.any(Number));
@@ -18,17 +19,17 @@ test('creaetes an engineer object', () => {
     expect(engineer.role).toEqual(expect.any(String));
     expect(engineer.github).toEqual(expect.any(String));
 
-    console.log(engineer.school);
+    console.log(engineer.id);
 });
 
 test('getGithub() returns github from user provided value', () => {
-    const engineer = new Engineer('Steve');
+    const engineer = new Engineer('Steve', 4567, 'mail@mail.mail', 'Engineer', 'github.com');
 
     expect(engineer.getGithub()).toEqual(expect.any(Object));
 });
 
 test("getRole() returns 'Engineer' ", () => {
-    const engineer = new Engineer('Steve');
+    const engineer = new Engineer('Steve', 4567, 'mail@mail.mail', 'Engineer', 'github.com');
     
-    expect(engineer.getRole()).toEqual(expect.any(Object));
+    expect(engineer.getRole()).toEqual(expect.any(String));
 });
