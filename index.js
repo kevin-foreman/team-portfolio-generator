b277a0200bb314180e0f97e3e6bd85715bfa1123
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-04-13 11:29:33 -0500

    refactored email validation and finalized manager prompts, successful print to console.log

--- index.js
+++ index.js
@@ -1,3 +1,143 @@
-const Team = require('./lib/team-app');
+// Need all the dependencies here
+const inquirer = require('inquirer');
+// const Employee = require('./Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const fs = require('fs');
+const generateHTML = require('./src/generate-team-page');
 
-new Team().buildTeam();
+
+
+// Set up empty array to hold team member information
+const teamMemberArray = [];
+
+
+// question prompts (format from inquirer)
+// function to start the Team builder and prompt user for team members...
+// starting with the Manager
+
+// Team.prototype.buildTeam = function() { // tried this way first, but const function would be cleaner
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: `What is the team manager's name? (required)`,
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log(`Please enter the team manager's name.`);
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: `Please enter the team manager's ID number (required)`,
+            validate: nameInput => {
+                // Reject if not a number (NaN)
+                
+                if (isNaN(nameInput)) {
+                    console.log('ID number is required');
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: `Please enter the team manager's email address (required)`,
+            validate: email => {
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+                if (valid) {
+                    return true;
+                } else {
+                    console.log('Please enter a valid email address');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'office-number',
+            message: `Please enter the team manager's office number`,
+            validate: nameInput => {
+                if (nameInput === null) {
+                    console.log('Please enter a phone number');
+                    return false;
+                } else {
+                    
+                    return true;
+                }
+                }
+        },
+    ])
+     // use a push to get all the answers
+     .then(managerInfo => {
+         const { name, id, email, officeNumber } = managerInfo;
+         const manager = new Manager (name, id, email, officeNumber);
+
+         teamMemberArray.push(manager);
+         console.log(manager);
+     })
+};
+
+promptUser();
+        // {
+        //     type: 'list',
+        //     name: 'employee-selection',
+        //     message: 'Which type of employee would you like to add to your team?',
+        //     choices: ['Engineer', 'Intern']
+        // }
+        // .then // handle the user inputs to build out the manager.js
+        // // 
+        
+
+        //     // const newEmployee = () => {
+        //     .then switch (newEmployee) {
+        //     case "Engineer":
+        //         inquirer
+        //         .prompt([
+        //             {
+        //                 type: 'input',
+        //                 name: 'id-number',
+        //                 message: 'Enter Engineer employee ID number.'
+        //             },
+        //             {
+        //                 type: 'input',
+        //                 name: 'github',
+        //                 message: 'Enter the Engineer Github username.'
+        //             }
+        //         ])
+        //         .then(function() {
+        //             newEmployee.push()
+        //         });
+        //         break;
+        //         case "Intern":
+        //             inquirer
+        //             .prompt([
+        //             {
+        //                 type: 'input',
+        //                 name: 'id-number',
+        //                 message: 'Enter the intern school.'
+        //             }    
+        //             ]).then(function() {
+        //                 newEmployee.push()
+        //             }
+// call promptUser
+// after all the information has been captured, print the captured array to the base html
+
+// promptUser()
+//         .then(addEmployee)
+//         .then(teamMemberArray => {
+//             return generateHTML(teamMemberArray);
+//         })
+//         // using fs to write the file
+//         .then(pageHTML => {
+//             return fs.writeFile(pageHTML);
+//         })
--- src/generate-team-page.js
+++ src/generate-team-page.js
@@ -3,6 +3,8 @@
 
 
     // create the Employee sections
+    // Manager class generater
+    // Only one manager is required for the team
     const generateManager = function (manager) {
         return `
     <div class = "col-4 mt-4>
@@ -15,20 +17,6 @@
 
 // Engineer class generator
 const generateEngineer = function (engineer) {
-    return `
-    <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-
-</body>`
-}
-
-// Manager class generator
-const generateManager = function (manager) {
     return `
     <head>
     <meta charset="UTF-8">
