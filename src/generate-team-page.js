5b20e8cdabfd3d439d1bb16e8b5f178de882f118
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-04-13 13:48:46 -0500

    Finished template html in JS template literals for 3 employee classes

--- src/generate-team-page.js
+++ src/generate-team-page.js
@@ -6,27 +6,58 @@
     // Manager class generater
     // Only one manager is required for the team
     const generateManager = function (manager) {
-        return `
+    return `
     <div class = "col-4 mt-4>
         <div class="card h-100">
-    <h3>${manager.name}</h3>
+            <div class="card-header">
+                <h3>${manager.name}</h3>
+                <h4>Manager</h4><i class="material-icons">content_paste</i>
+            </div>
+            <div class="card-body">
+                <p class="id">ID: ${manager.id}</p>
+                <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
+                <p class="office">Office Number: ${manager.officeNumber}</p>
+            </div>
         </div>
     </div>
     `;
-};
+}
 
 // Engineer class generator
 const generateEngineer = function (engineer) {
     return `
-    <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+    <div class = "col-4 mt-4>
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${engineer.name}</h3>
+                <h4>Manager</h4><i class="material-icons">content_paste</i>
+            </div>
+            <div class="card-body">
+                <p class="id">ID: ${engineer.id}</p>
+                <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+                <p class="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+            </div>
+        </div>
+    </div>
+    `;
+}
 
-</body>`
+const generateIntern = function (intern) {
+    return `
+    <div class = "col-4 mt-4>
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${intern.name}</h3>
+                <h4>Manager</h4><i class="material-icons">content_paste</i>
+            </div>
+            <div class="card-body">
+                <p class="id">ID: ${intern.id}</p>
+                <p class="email">Email: <a href="mailto:${intern.email}">${intern.email}</a></p>
+                <p class="office">Office Number: ${intern.school}</p>
+            </div>
+        </div>
+    </div>
+    `;
 }
 
 generateHTML = (data) => {
