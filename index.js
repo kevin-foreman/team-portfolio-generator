3d95c8e2bf2be419cbaf9ba6e189fa2f33a097c2
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-04-13 12:11:29 -0500

    fixing bugs in user prompt code

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const teamMemberArray = [];
 // function to start the Team builder and prompt user for team members...
 // starting with the Manager
 
-// Team.prototype.buildTeam = function() { // tried this way first, but const function would be cleaner
+// Team.prototype.buildTeam = function() { // tried this way first, but const arrow function is cleaner
 const promptUser = () => {
     return inquirer.prompt([
         {
@@ -76,61 +76,121 @@ const promptUser = () => {
                 }
                 }
         },
-    ])
+])
      // use a push to get all the answers
-     .then(managerInfo => {
-         const { name, id, email, officeNumber } = managerInfo;
-         const manager = new Manager (name, id, email, officeNumber);
+    .then(managerInfo => {
+        const { name, id, email, officeNumber } = managerInfo;
+        const manager = new Manager (name, id, email, officeNumber);
 
-         teamMemberArray.push(manager);
-         console.log(manager);
-     })
+        teamMemberArray.push(manager);
+        console.log(manager);
+    })
 };
 
-promptUser();
-        // {
-        //     type: 'list',
-        //     name: 'employee-selection',
-        //     message: 'Which type of employee would you like to add to your team?',
-        //     choices: ['Engineer', 'Intern']
-        // }
-        // .then // handle the user inputs to build out the manager.js
-        // // 
-        
+// add employees to the team
 
-        //     // const newEmployee = () => {
-        //     .then switch (newEmployee) {
-        //     case "Engineer":
-        //         inquirer
-        //         .prompt([
-        //             {
-        //                 type: 'input',
-        //                 name: 'id-number',
-        //                 message: 'Enter Engineer employee ID number.'
-        //             },
-        //             {
-        //                 type: 'input',
-        //                 name: 'github',
-        //                 message: 'Enter the Engineer Github username.'
-        //             }
-        //         ])
-        //         .then(function() {
-        //             newEmployee.push()
-        //         });
-        //         break;
-        //         case "Intern":
-        //             inquirer
-        //             .prompt([
-        //             {
-        //                 type: 'input',
-        //                 name: 'id-number',
-        //                 message: 'Enter the intern school.'
-        //             }    
-        //             ]).then(function() {
-        //                 newEmployee.push()
-        //             }
-// call promptUser
+const addEmployee = () => {
+
+    return inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'role',
+            message: "What is the employee's role?",
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is $({role.choice} + 's) name?",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log ("Employee name is required");
+                    return false; 
+                }
+            }
+        },
+
+        {
+            type: 'input',
+            name: 'id',
+            message: "Enter the employee's ID number",
+            validate: nameInput => {
+                // Using NaN again
+                if (isNaN(nameInput)) {
+                    console.log("ID number can only be a number")
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Enter the employee's email.",
+            validate: email => {
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                if (valid) {
+                    return true;
+                } else {
+                    console.log('Not a valid email')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "Enter the engineer's github username",
+            when: (input) => input.role === "Engineer",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Engineer's github username is required")
+                    // return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "Enter the intern's school",
+            when: (input) => input.role === "Intern",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Intern's school is required")
+                }
+            }
+        },
+
+        {
+            type: 'confirm',
+            name: 'confirmAddEmployee',
+            message: 'Do you want to add more team members?',
+            default: false
+        }
+])
 // after all the information has been captured, print the captured array to the base html
+.then(employeeData => {
+
+    const { name, id, email, role, github, school, confirmAddEmployee } = employeeData;
+    const employee;
+
+    if (role === "Engineer") {
+        employee = new Engineer (name, id, email, github);
+        console.log(employee);
+
+    }
+})
+
+promptUser();
+
+// call promptUser
 
 // promptUser()
 //         .then(addEmployee)
